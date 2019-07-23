8f41ff9553f4c73d9384664370018ac63ada093c
Willian <wsilvadev@gmail.com>
2019-07-23 14:28:48 -0300

    aplication succssesfully done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import ReactCalculator from './src/ReactCalculator';    
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => ReactCalculator);
